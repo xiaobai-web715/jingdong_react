dc3841878653fb213e041a48a72c667a6a89496e
xiaobai-web715 <liuxinghua715@163.com>
2021-07-31 13:55:32 +0800

    首页布局

--- JDFrontend/src/page/home/homeRouter/HomeIndex.js
+++ JDFrontend/src/page/home/homeRouter/HomeIndex.js
@@ -1,10 +1,98 @@
-import React from 'react'
+import React , {useEffect}from 'react'
+import '../../../assets/css/common/home/home.css'
+
+//这里用到的是swiper组件,究竟推不推荐企业级开发使用，之后再说
+import '../../../assets/css/common/swiper/swiper.min.css'
+import Swiper from '../../../assets/js/libs/swiper.min.js'
 
 const HomeIndex = () => {
+    console.log('1')
+    // 目前的理解,useEffect是在dom树加载完成之后，才会去执行
+    useEffect(()=>{
+        console.log('2')
+        new Swiper('.swiper-wrap' , {
+            autoplay : 3000,
+            pagination : '.swiper-pagination',
+            autoplayDisableOnInteraction : false,
+        })
+    } , [])
+    console.log('3')
     return (
-        <div>
-            首页
-        </div>
+        <>
+            {/* 顶部搜索 */}
+            <div>
+                <div className='search-header red-bg'>
+                    <div className='classify-icon'></div>
+                    <div className = 'search-wrap'>
+                        <div className = 'search-icon'></div>
+                        <div className = 'search-text'>请输入宝贝名称</div>
+                    </div>
+                    <div className = 'login-wrap'>
+                        <div className='login-text'>登录</div>
+                    </div>
+                </div>
+            </div>
+            {/* 幻灯片 */}
+            <div className='swiper-wrap'>
+                <div className='swiper-wrapper'>
+                    <div className='swiper-slide'><img className='image1' alt=''></img></div>
+                    <div className='swiper-slide'><img className='image2' alt=''></img></div>
+                    <div className='swiper-slide'><img className='image3' alt=''></img></div>
+                </div>
+                <div className='swiper-pagination'></div>
+            </div>
+            {/* 中间部分导航 */}
+            <div className='quick-nav'>
+                <ul className='item'>
+                    <li className='item-img'><img src='' alt=''></img></li>
+                    <li className='item-text'>潮流女装</li>
+                </ul>
+                <ul className='item'>
+                    <li className='item-img'><img src='' alt=''></img></li>
+                    <li className='item-text'>潮流女装</li>
+                </ul>
+                <ul className='item'>
+                    <li className='item-img'><img src='' alt=''></img></li>
+                    <li className='item-text'>潮流女装</li>
+                </ul>
+                <ul className='item'>
+                    <li className='item-img'><img src='' alt=''></img></li>
+                    <li className='item-text'>潮流女装</li>
+                </ul>
+            </div>
+            {/* 产品分类 */}
+            <div className='goods-level-wrap'>
+                <div className='classify-title color1'>—— 潮流女装 ——</div>
+                <div className='goods-level1-wrap'>
+                    <div className='goods-level1-item0'>
+                        <div className='goods-title'>高跟鞋女2018新款春季单鞋仙女甜美链子尖头防水台细跟女鞋一字带</div>
+                        <div className='goods-text'>精品打折</div>
+                        <div className='goods-price'>128元</div>
+                        <div className='goods-img'><img alt=''></img></div>
+                    </div>
+                    <div className='goods-level1-item1'>
+                        <div className='goods-row'>
+                            <div className='goods-row-title'>欧美尖头蝴蝶结拖鞋女夏外穿2018新款绸缎面细跟凉拖半拖鞋穆勒鞋</div>
+                            <div className='goods-row-text'>品质精挑</div>
+                            <div className='goods-row-img'><img alt=''></img></div>
+                        </div>
+                        <div className='goods-row'>
+                            <div className='goods-row-title'>欧美尖头蝴蝶结拖鞋女夏外穿2018新款绸缎面细跟凉拖半拖鞋穆勒鞋</div>
+                            <div className='goods-row-text'>品质精挑4折起</div>
+                            <div className='goods-row-img'><img alt=''></img></div>
+                        </div>
+                    </div>
+                </div>
+                <div className='goods-list-wrap'>
+                    <div className='goods-list'>
+                        <div className='title'>小白鞋女2018春夏季新款韩版百搭平底学生原宿ulzzang帆布鞋板鞋</div>
+                    </div>
+                    <div className='goods-list'></div>
+                    <div className='goods-list'></div>
+                    <div className='goods-list'></div>
+                </div>
+            </div>
+        </>
     )
 }
 
--- JDFrontend/src/page/home/jdIndex/Index.js
+++ JDFrontend/src/page/home/jdIndex/Index.js
@@ -1,6 +1,6 @@
 import React , {lazy , Suspense , useState , useEffect} from 'react';
 import  "../../../assets/css/common/public/public.css"
-import  "../../../assets/css/common/home/home.css"
+import  "../../../assets/css/common/Index/Index.css"
 import {Route , Switch} from 'react-router-dom'
 import config from '../../../assets/js/conf/config'
 
@@ -12,10 +12,11 @@ const JDIndex = (props) => {
     // 这里传入的props会因为url的变化，而改变，当props改变时，就会触发useEffect执行(这里要注意，无论是props改变，还是useState的状态改变，都是刷新组件，并不会重新挂载组件，挂载组件的这个过程已经在最开始完成了)
     // console.log('props' , props)//这也就是打印台为什么会一次执行两遍console.log的原因
     const [stateStyle , setStateStyle] = useState({})
-    console.log('stateStyle' , stateStyle)
+    // console.log('stateStyle' , stateStyle)
     // {}这里最初不用赋值，因为执行到useEffect的时候会改变这个状态，再重新加载组件
     const goPage = (pUrl) =>{
         props.history.replace(config.path + pUrl);
+        // replace的作用是不让其添加到历史记录里面去
         // props.history.push(config.path + pUrl);
         // 如果用的是push的话，当点击同一个地方两次的时候，会有一个警告
         //这里必须加入这个config.path才不会出错(不加上的话，就会一直在前面添加根路径，也就是jd)(目前还不清楚为啥)
