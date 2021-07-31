a2763858a102330fe9f965b0974a8514ac742af2
xiaobai-web715 <liuxinghua715@163.com>
2021-07-31 16:22:12 +0800

    首页布局

--- JDFrontend/src/page/home/homeRouter/HomeIndex.js
+++ JDFrontend/src/page/home/homeRouter/HomeIndex.js
@@ -18,7 +18,7 @@ const HomeIndex = () => {
     } , [])
     console.log('3')
     return (
-        <>
+        <div className='page'>
             {/* 顶部搜索 */}
             <div>
                 <div className='search-header red-bg'>
@@ -67,7 +67,7 @@ const HomeIndex = () => {
                     <div className='goods-level1-item0'>
                         <div className='goods-title'>高跟鞋女2018新款春季单鞋仙女甜美链子尖头防水台细跟女鞋一字带</div>
                         <div className='goods-text'>精品打折</div>
-                        <div className='goods-price'>128元</div>
+                        <div className='goods-price1'>128元</div>
                         <div className='goods-img'><img alt=''></img></div>
                     </div>
                     <div className='goods-level1-item1'>
@@ -86,13 +86,151 @@ const HomeIndex = () => {
                 <div className='goods-list-wrap'>
                     <div className='goods-list'>
                         <div className='title'>小白鞋女2018春夏季新款韩版百搭平底学生原宿ulzzang帆布鞋板鞋</div>
+                        <div className='image'><img alt=''></img></div>
+                        <div className='price'>￥288</div>
+                        <div className='unprice'>￥388</div>
                     </div>
-                    <div className='goods-list'></div>
-                    <div className='goods-list'></div>
-                    <div className='goods-list'></div>
+                    <div className='goods-list'>
+                        <div className='title'>小白鞋女2018春夏季新款韩版百搭平底学生原宿ulzzang帆布鞋板鞋</div>
+                        <div className='image'><img alt=''></img></div>
+                        <div className='price'>￥288</div>
+                        <div className='unprice'>￥388</div>
+                    </div>
+                    <div className='goods-list'>
+                        <div className='title'>小白鞋女2018春夏季新款韩版百搭平底学生原宿ulzzang帆布鞋板鞋</div>
+                        <div className='image'><img alt=''></img></div>
+                        <div className='price'>￥288</div>
+                        <div className='unprice'>￥388</div>
+                    </div>
+                    <div className='goods-list'>
+                        <div className='title'>小白鞋女2018春夏季新款韩版百搭平底学生原宿ulzzang帆布鞋板鞋</div>
+                        <div className='image'><img alt=''></img></div>
+                        <div className='price'>￥288</div>
+                        <div className='unprice'>￥388</div>
+                    </div>
                 </div>
             </div>
-        </>
+            <div className='goods-level-wrap'>
+                <div className='classify-title color2'>—— 品牌男装 ——</div>
+                <div className='goods-level1-wrap'>
+                    <div className='goods-level1-item0'>
+                        <div className='goods-title2'>高跟鞋女2018新款春季单鞋仙女甜美链子尖头防水台细跟女鞋一字带</div>
+                        <div className='goods-text2'>火爆开售</div>
+                        <div className='goods-img2'><img alt=''></img></div>
+                    </div>
+                    <div className='goods-level1-item0'>
+                        <div className='goods-title2'>高跟鞋女2018新款春季单鞋仙女甜美链子尖头防水台细跟女鞋一字带</div>
+                        <div className='goods-text2'>火爆开售</div>
+                        <div className='goods-img2'><img alt=''></img></div>
+                    </div>
+                </div>
+                <div className='goods-list-wrap'>
+                    <div className='goods-list'>
+                        <div className='title'>小白鞋女2018春夏季新款韩版百搭平底学生原宿ulzzang帆布鞋板鞋</div>
+                        <div className='image'><img alt=''></img></div>
+                        <div className='price'>￥288</div>
+                        <div className='unprice'>￥388</div>
+                    </div>
+                    <div className='goods-list'>
+                        <div className='title'>小白鞋女2018春夏季新款韩版百搭平底学生原宿ulzzang帆布鞋板鞋</div>
+                        <div className='image'><img alt=''></img></div>
+                        <div className='price'>￥288</div>
+                        <div className='unprice'>￥388</div>
+                    </div>
+                    <div className='goods-list'>
+                        <div className='title'>小白鞋女2018春夏季新款韩版百搭平底学生原宿ulzzang帆布鞋板鞋</div>
+                        <div className='image'><img alt=''></img></div>
+                        <div className='price'>￥288</div>
+                        <div className='unprice'>￥388</div>
+                    </div>
+                    <div className='goods-list'>
+                        <div className='title'>小白鞋女2018春夏季新款韩版百搭平底学生原宿ulzzang帆布鞋板鞋</div>
+                        <div className='image'><img alt=''></img></div>
+                        <div className='price'>￥288</div>
+                        <div className='unprice'>￥388</div>
+                    </div>
+                </div>
+            </div>
+            <div className='goods-level-wrap'>
+                <div className='classify-title color3'>—— 电脑办公 ——</div>
+                <div className='goods-level1-wrap'>
+                    <div className='goods-level1-item0'>
+                        <div className='goods-title'>高跟鞋女2018新款春季单鞋仙女甜美链子尖头防水台细跟女鞋一字带</div>
+                        <div className='goods-text'>精品打折</div>
+                        <div className='goods-price1'>128元</div>
+                        <div className='goods-img'><img alt=''></img></div>
+                    </div>
+                    <div className='goods-level1-item1'>
+                        <div className='goods-row'>
+                            <div className='goods-row-title'>欧美尖头蝴蝶结拖鞋女夏外穿2018新款绸缎面细跟凉拖半拖鞋穆勒鞋</div>
+                            <div className='goods-row-text'>品质精挑</div>
+                            <div className='goods-row-img'><img alt=''></img></div>
+                        </div>
+                        <div className='goods-row'>
+                            <div className='goods-row-title'>欧美尖头蝴蝶结拖鞋女夏外穿2018新款绸缎面细跟凉拖半拖鞋穆勒鞋</div>
+                            <div className='goods-row-text'>品质精挑4折起</div>
+                            <div className='goods-row-img'><img alt=''></img></div>
+                        </div>
+                    </div>
+                </div>
+                <div className='goods-list-wrap'>
+                    <div className='goods-list'>
+                        <div className='title'>小白鞋女2018春夏季新款韩版百搭平底学生原宿ulzzang帆布鞋板鞋</div>
+                        <div className='image'><img alt=''></img></div>
+                        <div className='price'>￥288</div>
+                        <div className='unprice'>￥388</div>
+                    </div>
+                    <div className='goods-list'>
+                        <div className='title'>小白鞋女2018春夏季新款韩版百搭平底学生原宿ulzzang帆布鞋板鞋</div>
+                        <div className='image'><img alt=''></img></div>
+                        <div className='price'>￥288</div>
+                        <div className='unprice'>￥388</div>
+                    </div>
+                    <div className='goods-list'>
+                        <div className='title'>小白鞋女2018春夏季新款韩版百搭平底学生原宿ulzzang帆布鞋板鞋</div>
+                        <div className='image'><img alt=''></img></div>
+                        <div className='price'>￥288</div>
+                        <div className='unprice'>￥388</div>
+                    </div>
+                    <div className='goods-list'>
+                        <div className='title'>小白鞋女2018春夏季新款韩版百搭平底学生原宿ulzzang帆布鞋板鞋</div>
+                        <div className='image'><img alt=''></img></div>
+                        <div className='price'>￥288</div>
+                        <div className='unprice'>￥388</div>
+                    </div>
+                </div>
+            </div>
+            <div className='rec-title-wrap'>
+                <div className='line'></div>
+                <div className='reco-text-wrap'>
+                    <div className='reco-icon'></div>
+                    <div className='reco-text'>为你推荐</div>
+                </div>
+                <div className='line'></div>
+            </div>
+            <div className='reco-item-wrap'>
+                <div className='reco-item'>
+                    <div className='image'><img alt=''></img></div>
+                    <div className='title'>ONLY冬装新品雪纺拼接流苏腰带长宽连衣裙女</div>
+                    <div className='price'>￥399</div>
+                </div>
+                <div className='reco-item'>
+                    <div className='image'><img alt=''></img></div>
+                    <div className='title'>ONLY冬装新品雪纺拼接流苏腰带长宽连衣裙女</div>
+                    <div className='price'>￥399</div>
+                </div>
+                <div className='reco-item'>
+                    <div className='image'><img alt=''></img></div>
+                    <div className='title'>ONLY冬装新品雪纺拼接流苏腰带长宽连衣裙女</div>
+                    <div className='price'>￥399</div>
+                </div>
+                <div className='reco-item'>
+                    <div className='image'><img alt=''></img></div>
+                    <div className='title'>ONLY冬装新品雪纺拼接流苏腰带长宽连衣裙女</div>
+                    <div className='price'>￥399</div>
+                </div>
+            </div>
+        </div>
     )
 }
 
